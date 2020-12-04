273fa569e3cebb10360add39be81faff6f3015e8
Tarush27 <tarushkaistha05@gmail.com>
2020-12-04 17:39:14 +0530

    Added up action bar.

--- app/src/main/java/com/example/bmiapp/SecondActivity.java
+++ app/src/main/java/com/example/bmiapp/SecondActivity.java
@@ -14,6 +14,9 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     public void calculatebmi(View v){

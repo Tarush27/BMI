21bf7339dab08d8ac6bf7b1159f6592bba55cbcf
Tarush27 <tarushkaistha05@gmail.com>
2020-12-04 17:45:32 +0530

    Rating based on the performance.

--- app/src/main/java/com/example/bmiapp/Rating.java
+++ app/src/main/java/com/example/bmiapp/Rating.java
@@ -6,26 +6,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class Rating extends AppCompatActivity {
 
+    RatingBar ratingBar;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
-    }
-    public void enter(View v){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setIcon(R.drawable.ic_thank);
-        builder.setTitle("Thank you for your feedback");
-        builder.setMessage("Your rating is : ");
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ratingBar = findViewById(R.id.ratingBar);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
+            public void onClick(View v) {
+                String s = String.valueOf(ratingBar.getRating());
+                Toast.makeText(getApplicationContext(), s + " Star, Keep Working !!",Toast.LENGTH_SHORT).show();
             }
         });
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
     }
 }
